ff841c8b083c652c69ffe50699cfe73c9de9b5ba
Eric Lowber <elowber@gmail.com>
2015-01-24 23:49:57 +0000

    changed incoming controller bookmark creation

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -30,7 +30,7 @@ class IncomingController < ApplicationController
         topic.save
      end
      # Check if the topic is nil, if so, create and save a new topic
-    bookmark = Bookmark.new(url: url)
+    bookmark = Bookmark.new(url: url, user: user, topic: topic)
     bookmark.save
      # Now that we're sure we have a valid user and topic, build and save a new bookmark
 
