41aaf51a8ae7178c3b3e6d8666daf0940c6151a2
Eric Lowber <elowber@gmail.com>
2015-01-25 01:47:50 +0000

    incoming controller change

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -14,7 +14,8 @@ class IncomingController < ApplicationController
 
      user = User.find_by_email(params[:sender])  
      topic = Topic.find_by_name(params[:subject])
-     body = params[:"body-plain"]
+
+     body = params["body-html"]
      url = body.scan(/https?:\/\/[\S]+/)
      # url = /.*(http:\/\/.*|https:\/\/.*)".*/.match(params[stripped-html])[1]
      # Check if user is nil, if so, create and save a new user
