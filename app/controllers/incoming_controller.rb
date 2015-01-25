2a5b10b559e0a6d65ac4bd61da4519082b534316
Eric Lowber <elowber@gmail.com>
2015-01-25 18:42:44 +0000

    another try on incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -15,9 +15,10 @@ class IncomingController < ApplicationController
      user = User.find_by_email(params[:sender])  
      topic = Topic.find_by_name(params[:subject])
 
-     url = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.match(params[stripped-html])[1]
+     url = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.match(params[:'stripped-html'])[1]
    
     
+
 
      # Check if user is nil, if so, create and save a new user
      if user.nil? 
