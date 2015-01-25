a40843f35d696481dc1a4a1e54d03ad08ddfd77f
Eric Lowber <elowber@gmail.com>
2015-01-25 20:54:26 +0000

    using stripped html in by incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -14,9 +14,8 @@ class IncomingController < ApplicationController
 
      user = User.find_by_email(params[:sender])  
      topic = Topic.find_by_name(params[:subject])   
-    
-     url = URI.extract(params["stripped-text"]).first
-
+     url = URI.extract(params["stripped-html"]).first
+     
      # Check if user is nil, if so, create and save a new user
      if user.nil? 
         user = User.new(
